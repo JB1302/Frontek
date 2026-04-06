895d6baedc91e64ceb2dba6e436ebb34359be8a1
Alejandro <gonzalez-a26@hotmail.com>
2026-04-06 17:49:34 -0600

    Estructura de Migracion

---
+++ Frontek Full Web E-Commerce.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontek_Full_Web_E_Commerce.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Frontek Full Web E-Commerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Frontek_Full_Web_E_Commerce.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        /*
+            Aqui como antes haciamos en DbContext
+         
+            Ejemplo
+
+            public DbSet<Producto> Productos { get; set; }
+         
+         */
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
