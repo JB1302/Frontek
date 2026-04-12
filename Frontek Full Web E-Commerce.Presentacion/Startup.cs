d5ed60ab9ff8e73a60d026a21ae9cce8bb63d0c1
JB1302 <jstevenbarrantes@gmail.com>
2026-04-11 23:28:53 -0600

    Creacion de Presentacion + Migracion de Views del proyecto original Y nuggets de Seguridad.

---
+++ Frontek Full Web E-Commerce.Presentacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Frontek_Full_Web_E_Commerce.Presentacion.Startup))]
+namespace Frontek_Full_Web_E_Commerce.Presentacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
