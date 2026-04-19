ca7767f2d8af925093b346dc991817513510f031
JB1302 <jstevenbarrantes@gmail.com>
2026-04-18 23:24:37 -0600

    Configuracion Inicial de Web API

---
+++ Frontek Web API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Frontek_Web_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Frontek Web API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Frontek_Web_API.WebApiApplication" Language="C#" %>
